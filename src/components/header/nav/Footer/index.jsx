4ff5e19030545a09c8fcb766ac8a100c6fc21de2
bbwebdevelop <bbwebdevelop@gmail.com>
2024-02-22 15:51:42 +0100

    curved_menu

---
+++ src/components/header/nav/Footer/index.jsx
@@ -0,0 +1,12 @@
+import styles from './style.module.scss';
+
+export default function index() {
+  return (
+    <div className={styles.footer}>
+        <a>Facebook</a>
+        <a>Instagram</a>
+        <a>Github</a>
+        <a>LinkedIn</a>
+    </div>
+  )
+}
